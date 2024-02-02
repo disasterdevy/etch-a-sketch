37ed20c508071236eadecbf0c717c9fe1923978f
disasterdevy <36893414+disasterdevy@users.noreply.github.com.>
2024-02-02 20:12:08 +0100

    Add createGrid(), adds divs to main container

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let amountDiv = 0;
+const container = document.querySelector('.container');
+
+function createGrid(){
+    const contentGrid = document.createElement('div');
+    contentGrid.textContent = 'DeppDummi';
+    container.appendChild(contentGrid);
+}
