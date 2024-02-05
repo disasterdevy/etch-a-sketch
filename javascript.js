a69990dc7e96eeebe1307577a8154a53a34ae3e4
disasterdevy <36893414+disasterdevy@users.noreply.github.com.>
2024-02-05 10:52:02 +0100

    Add feature user can input size of grid after clicking button

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
-let amountDiv = 1024;
+// let amountDiv = 10000;
 const container = document.querySelector('.container');
+const inputButton = document.querySelector('.inputButton');
+let contentGrid;
+let amountDiv = 0;
+
+
+inputButton.addEventListener('click', () =>{
+    amountDiv = prompt("Please enter the size of your grid: ");
+    createAmountEntered();
+    contentGrid = document.querySelectorAll('.contentGrid');
+    const listLength = contentGrid.length;
+    addHoveringEffect();
+})
 
 function createGrid(){
     const contentGrid = document.createElement('div');
@@ -14,14 +26,15 @@ function createAmountEntered(){
     }
 }
 
-createAmountEntered();
-const contentGrid = document.querySelectorAll('.contentGrid');
-const listLength = contentGrid.length;
+// const contentGrid = document.querySelectorAll('.contentGrid');
+// const listLength = contentGrid.length;
 
 
 // find out how this magic works! 
-Array.prototype.forEach.call(contentGrid, item => {
-    item.addEventListener('mouseenter', () =>{
-        item.classList.add('contentHovered');
-    })      
-})
+function addHoveringEffect(){
+    Array.prototype.forEach.call(contentGrid, item => {
+        item.addEventListener('mouseenter', () =>{
+            item.classList.add('contentHovered');
+        })      
+    })
+}
